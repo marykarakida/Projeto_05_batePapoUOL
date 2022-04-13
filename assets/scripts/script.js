59637b50faf77dcdb3e391ea1aa4f73a5e9dd4c2
Mary Karakida <makarakida@gmail.com>
2022-04-13 08:29:15 -0300

    requisito: chat

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -8,16 +8,30 @@ function perguntarNome() {
 }
 
 function conferirNome() {
-    const promise = axios.get("https://mock-api.driven.com.br/api/v6/uol/participants");
     const nomeServidor = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", nome);
     nomeServidor.then(manterConexao);
+    //no bonus, aki funcao para atualizar lista de contatos
     nomeServidor.catch(corrigirNome);
 }
 
+// como mandar mensagem de saida
 function manterConexao() {
-    const conectado = setInterval(function() {
+    const mensagemEntrada = {
+        from: nome.name,
+		to: "Todos",
+		text: "entra na sala...",
+		type: "status",
+		time: Date.now()
+    }
+    const entrada = axios.post("https://mock-api.driven.com.br/api/v6/uol/messages", mensagemEntrada);
+
+    const conexao = setInterval(function() {
         const conexaoServidor = axios.post("https://mock-api.driven.com.br/api/v6/uol/status", nome);
-    },5000)
+    },5000);
+    const chat = setInterval(function() {
+        const chatServidor = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
+        chatServidor.then(carregarMensagens);
+    },3000);
 }
 
 function corrigirNome(erro) {
@@ -25,4 +39,20 @@ function corrigirNome(erro) {
         alert("Esse nome já está sendo usado!")
         perguntarNome();
     }
+}
+
+function carregarMensagens(chatServidor) {
+    const chat = document.querySelector(".chat");
+    chat.innerHTML = ""
+    const chatLista = chatServidor.data;
+    for (var i = 0; i < chatLista.length; i++) {
+        if (chatLista[i].type === "status") {
+            chat.innerHTML += `<li class="mensagem mensagem-de-status"><span class="relogio">${chatLista[i].time}</span> <span class="usuario">${chatLista[i].from}</span> ${chatLista[i].text}</li>`
+        } else if (chatLista[i].type === "message") {
+            chat.innerHTML += `<li class="mensagem mensagem-normal"><span class="relogio">${chatLista[i].time}</span> <span class="usuario">${chatLista[i].from}</span> para <span class="usuario">${chatLista[i].to}</span>: ${chatLista[i].text}</li>`
+        } else if (chatLista[i].type === "private_message" && chatLista[i].to === nome.name) {
+            chat.innerHTML += `<li class="mensagem mensagem-reservada"><span class="relogio">${chatLista[i].time}</span> <span class="usuario">${chatLista[i].from}</span> reservadamente para <span class="usuario">${chatLista[i].to}</span>: ${chatLista[i].text}</li>`
+        }
+    }
+    chat.scrollIntoView(false);
 }
